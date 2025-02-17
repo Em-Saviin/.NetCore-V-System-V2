a8e23d0af9e81939b1e5e4005d8ba57e01525a6a
Em Savin <savin.em@PENGHUOTH.COM>
2025-02-17 17:34:33 +0700

    add model select2

---
+++ V-System Core/Models/CoreSelect2Item.cs
@@ -0,0 +1,9 @@
+﻿namespace V_System_Core.Models
+{ 
+        public class CoreSelect2Item
+        {
+            public int? id { get; set; }
+
+            public string? text { get; set; } 
+       }
+}
