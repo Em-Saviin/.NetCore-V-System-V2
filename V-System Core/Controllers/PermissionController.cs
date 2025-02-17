c9b4d9ea6e43b7d08ef08d207c0dcac342030060
Em Savin <savin.em@PENGHUOTH.COM>
2025-02-17 17:33:40 +0700

    add controller

---
+++ V-System Core/Controllers/PermissionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using V_System_Core.Component;
+using V_System_Core.Data;
+
+
+namespace V_System_Core.Controllers
+{
+    public class PermissionController : Controller
+    {
+        private readonly V_System_Core.Data.AppDbContext db;
+        public PermissionController(AppDbContext _dbContext)
+        {
+            this.db = _dbContext;   
+        } 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Block Action
+
+        public IActionResult GetDataSelect2()
+        {
+            var RoleData = StaticClass.GetSelect2Item(db , "ROLE" , 0);
+            var MenuData = StaticClass.GetSelect2Item(db , "MENU" , 0);
+            return Json(new { RoleData, MenuData });    
+        }
+
+        public IActionResult GetMenu(int menuId = 0, int roleId = 0)
+        {
+            var MenuData = StaticClass.GetSelect2Item(db, "MENU", 0);
+            return Json(new { data = MenuData });
+        }
+        public IActionResult GetListPermissionOnRole()
+        {
+            string sql = "SP_GET_MODULE_ON_ROLE_PERMISSION";
+            var rows = StaticClass.ExecSPWithoutParam(db, sql);
+            return Json(new {data = rows });
+        }
+    }
+}
